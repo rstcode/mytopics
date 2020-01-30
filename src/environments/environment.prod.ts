356889f312658ca05c22d919e85a6cde66e2add3
SAGAR TEJA <keeki.rsks@gmail.com>
2020-01-31 01:17:47 +0530

    add ignore files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "#################################",
-    authDomain: "#################################",
-    databaseURL: "#################################",
-    projectId: "#################################",
-    storageBucket: "#################################",
-    messagingSenderId: "#################################",
-    appId: "#################################",
-    measurementId: "#################################"
+    apiKey: "AIzaSyBuPhK9QW44jD8f8HJuoa2TUM411zSodXY",
+    authDomain: "myposts-rst007.firebaseapp.com",
+    databaseURL: "https://myposts-rst007.firebaseio.com",
+    projectId: "myposts-rst007",
+    storageBucket: "myposts-rst007.appspot.com",
+    messagingSenderId: "390114702304",
+    appId: "1:390114702304:web:d09070eb8e199aa03b3185",
+    measurementId: "G-201ZEXC4LQ"
   }
 };
